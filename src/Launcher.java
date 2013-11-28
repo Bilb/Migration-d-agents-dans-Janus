7c5be5bc7de9308802e1ceedcf4c4c93fcb55d03
Bilb <audric.bilb@gmail.com>
2013-11-28 14:39:46 +0100

    adding network support

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,17 @@
 import org.janusproject.kernel.Kernel;
 import org.janusproject.kernel.agent.Kernels;
+import org.janusproject.kernel.network.jxse.agent.JxtaJxseKernelAgentFactory;
 
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		Kernel k = Kernels.get(true);
-		
-		k.launchLightAgent(new TestAgent());
+		// The following line permits to create a JXTA
+		// standard-edition kernel.
+		Kernels.setPreferredKernelFactory(new JxtaJxseKernelAgentFactory());
+		Kernel kernel = Kernels.get(true);
+		TestAgent agent = new TestAgent();
+		kernel.launchHeavyAgent(agent,"agent1");		
+
 	}
 }
